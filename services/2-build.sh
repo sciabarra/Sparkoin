f8f00ed06828b6e9ef8cf57b8d4fc59603aa3904
Michele Sciabarra <michele+youtrack@sciabarra.com>
2016-03-28 18:58:34 +0200

    removed old files

--- services/2-build.sh
+++ services/2-build.sh
@@ -4,4 +4,3 @@ docker build -t sparkoin/spark:1 spark
 docker build -t sparkoin/kafka:1 kafka
 docker build -t sparkoin/cassandra:1 cassandra
 docker build -t sparkoin/zeppelin:1 zeppelin
-docker build -t sparkoin/bitcore:1 bitcore
