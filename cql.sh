47f9f69b406f1d1a4d4b52401684708d91c6e569
Michele Sciabarra <michele+youtrack@sciabarra.com>
2016-04-04 11:24:18 +0200

    moved things around

--- cql.sh
+++ cql.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-bash enter.sh sparkoin_cassandra0 "cd /app/apps/cql ; /app/cassandra/bin/cqlsh $*"
+bash enter.sh sparkoin_cassandra0 "cd /app/apps/etc/cql ; /app/cassandra/bin/cqlsh $*"
 
