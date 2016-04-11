15a21a8f25008b91fa40e5c07a5131cb81f2369e
Michele Sciabarra <michele+youtrack@sciabarra.com>
2016-04-11 14:37:40 +0200

    non working scala

--- services/1-download.sh
+++ services/1-download.sh
@@ -12,3 +12,7 @@ cd -
 cd kafka ; wget -nc http://www-eu.apache.org/dist/kafka/0.9.0.1/kafka_2.11-0.9.0.1.tgz ; cd -
 cd zeppelin ; wget -nc http://www-eu.apache.org/dist/incubator/zeppelin/0.5.6-incubating/zeppelin-0.5.6-incubating-bin-all.tgz ; cd -
 cd zeppelin ; wget -nc http://central.maven.org/maven2/com/google/guava/guava/14.0.1/guava-14.0.1.jar ; cd -
+cd jupyter
+#wget -nc https://oss.sonatype.org/content/repositories/snapshots/com/github/alexarchambault/jupyter/jupyter-scala-cli_2.11.6/0.2.0-SNAPSHOT/jupyter-scala_2.11.6-0.2.0-SNAPSHOT.zip
+wget -nc https://oss.sonatype.org/content/repositories/snapshots/com/github/alexarchambault/jupyter/jupyter-scala-cli_2.10.5/0.2.0-SNAPSHOT/jupyter-scala_2.10.5-0.2.0-SNAPSHOT.zip
+cd -
