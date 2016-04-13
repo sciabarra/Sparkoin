2f79287a19f1450804c202a2fca1cf7a3510b82d
Michele Sciabarra <michele+youtrack@sciabarra.com>
2016-04-13 17:28:13 +0200

    zeppelin, archived

---
+++ services/zeppelin/1-download.sh
@@ -0,0 +1,2 @@
+cd zeppelin ; wget -nc http://www-eu.apache.org/dist/incubator/zeppelin/0.5.6-incubating/zeppelin-0.5.6-incubating-bin-all.tgz ; cd -
+cd zeppelin ; wget -nc http://central.maven.org/maven2/com/google/guava/guava/14.0.1/guava-14.0.1.jar ; cd -
