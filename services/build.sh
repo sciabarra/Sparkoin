370d3b790ba9ee2f128120440f2fcea4fb8ba22f
Michele Sciabarra <michele+youtrack@sciabarra.com>
2016-04-18 06:39:30 +0000

    rebuild jupyter docker

--- services/build.sh
+++ services/build.sh
@@ -7,4 +7,4 @@ docker build -t sparkoin/spark:2 spark
 docker build -t sparkoin/kafka:2 kafka
 docker build -t sparkoin/cassandra:2 cassandra
 docker build -t sparkoin/bitcore:2 bitcore
-docker build -t sparkoin/jupyter:2 jupyter
+docker build -t sparkoin/jupyter:4 jupyter
