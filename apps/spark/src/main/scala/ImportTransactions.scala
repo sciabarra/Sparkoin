1187889f1efbc2ec897c554c387d1f861f469fce
Alessandro <alessandro.mongelli@gmail.com>
2016-04-19 14:45:52 +0100

    Sparkoin-14 Create a table with the address and the amount  - decoded the scripts using the bitcore api  - added address and decoded script to tx input and output types

--- apps/spark/src/main/scala/ImportTransactions.scala
+++ apps/spark/src/main/scala/ImportTransactions.scala
@@ -1,3 +1,4 @@
+import DataModel._
 import com.datastax.spark.connector._
 import kafka.serializer.{DefaultDecoder, StringDecoder}
 import org.apache.spark.streaming.kafka.KafkaUtils
@@ -18,26 +19,18 @@ object ImportTransactions extends App {
   val sc = new SparkContext(conf)
   val ssc = new StreamingContext(sc, Milliseconds(500))
 
-  case class BlockHeader(hash: String, version: Int, prev_hash: String, merkle_root: String, time: Long, bits: Long, nonce: Long)
-
-  case class Block(block_id: String, block_height: Int, tx_number: Int, difficulty: Double, header: BlockHeader)
-
-  case class TxInput(previous_tx_hash: String, output_tx_id: Long, script_sig: String, sequence_no: Long)
-
-  case class TxOutput(value: Long, script_pub_key: String)
-
-  case class Tx(tx_id: String, block_id:String, version_no: Int, tx_in_list: List[TxInput], tx_out_list: List[TxOutput], lock_time: Int)
-
   implicit val formats = Serialization.formats(ShortTypeHints(List(classOf[TxInput], classOf[TxOutput]))) +
     FieldSerializer[Tx](
-      renameTo("tx_id", "hash") orElse renameTo("version_no", "version") orElse renameTo("tx_in_list", "inputs") orElse renameTo("tx_out_list", "outputs") orElse renameTo("lock_time", "nLockTime"),
-      renameFrom("hash", "tx_id") orElse renameFrom("version", "version_no") orElse renameFrom("inputs", "tx_in_list") orElse renameFrom("outputs", "tx_out_list") orElse renameFrom("nLockTime", "lock_time")) +
+      renameTo("tx_id", "hash") orElse renameTo("version_no", "version")  orElse renameTo("lock_time", "nLockTime"),
+      renameFrom("hash", "tx_id") orElse renameFrom("version", "version_no") orElse renameFrom("nLockTime", "lock_time")) +
+/*
     FieldSerializer[TxInput](
       renameTo("previous_tx_hash", "prevTxId") orElse renameTo("output_tx_id", "outputIndex") orElse renameTo("script_sig", "script") orElse renameTo("sequence_no", "sequenceNumber"),
       renameFrom("prevTxId", "previous_tx_hash") orElse renameFrom("outputIndex", "output_tx_id") orElse renameFrom("script", "script_sig") orElse renameFrom("sequenceNumber", "sequence_no") orElse renameFrom("nlocktime", "lock_time")) +
     FieldSerializer[TxOutput](
       renameTo("value", "satoshis") orElse renameTo("script_pub_key", "script"),
       renameFrom("satoshis", "value") orElse renameFrom("script", "script_pub_key")) +
+*/
     FieldSerializer[BlockHeader](
       renameTo("prev_hash", "prevHash") orElse renameTo("merkle_root", "merkleRoot"),
       renameFrom("prevHash", "prev_hash") orElse renameFrom("merkleRoot", "merkle_root"))
@@ -84,15 +77,4 @@ object ImportTransactions extends App {
   ssc.start()
   ssc.awaitTermination()
 
-  /*
-    val collection = sc.parallelize(
-      Seq(
-        Tx("111111", 1, List(TxInput("aaaaa", 0, "hhh", 1)),List(TxOutput(5000,"pppp")) ,0),
-        Tx("222222", 1, List(TxInput("aaaaa", 0, "hhhh", 2)), List(TxOutput(50000,"ppppp")) ,0)))
-    collection.saveToCassandra("sparkoin", "tx", SomeColumns("tx_id", "version_no", "tx_in_list", "tx_out_list", "lock_time"))
-
-    val rdd = sc.cassandraTable("sparkoin", "tx")
-    println(rdd.count)
-    println(rdd.first)
-  */
 }
