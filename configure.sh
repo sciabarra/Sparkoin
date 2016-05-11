0edca7b317c1e4fffa9e167efb6ed3baff976cdf
Michele Sciabarra <michele@sciabarra.com>
2016-05-11 13:23:02 +0200

    use 500 for 0 uid

--- configure.sh
+++ configure.sh
@@ -5,10 +5,13 @@ exit 1
 else IP="$1"
 fi
 MYID=$(id -u)
+if test "$MYID" == "0"
+then MYID=500 
+fi
 echo $MYID >services/java/uid.txt
 echo $IP >services/java/ip.txt
 test -e services/ssh/id_rsa ||  ssh-keygen -t rsa -f services/ssh/id_rsa -N ''
 cp services/java/uid.txt services/jupyter/uid.txt
-docker volume create --name hadoop
+docker volume create --name cassandra
 docker volume create --name bitcore 
 echo You can now build your enviroment running build.sh
