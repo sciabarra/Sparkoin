8fa2ffefd70d870550cc034117fe0b627b2f2bcb
Michele Sciabarra <michele+youtrack@sciabarra.com>
2016-05-13 08:34:54 +0200

    updated configure  - no ip

--- configure.sh
+++ configure.sh
@@ -2,11 +2,15 @@
 if ! which docker-compose 2>&1 >/dev/null
 then echo "You need to install docker-compose" ; exit 1
 fi
-if test -z "$1" 
-then echo "Please specify your docker IP"
-exit 1
-else IP="$1"
-fi
+## suspended - needed for Kafka
+#if test -z "$1" 
+#then echo "Please specify your docker IP"
+#exit 1
+#else IP="$1"
+#fi
+#echo $IP >services/kafka/ip.txt
+## suspended - needed for Hadoop
+#test -e services/ssh/id_rsa ||  ssh-keygen -t rsa -f services/ssh/id_rsa -N ''
 MYID=$(id -u)
 if test "$MYID" == "0"
 then 
@@ -16,11 +20,10 @@ echo "Note you need docker 1.11 to run as non-root on RedHat systems."
 exit 1
 fi
 echo $MYID >services/java/uid.txt
-echo $IP >services/java/ip.txt
-test -e services/ssh/id_rsa ||  ssh-keygen -t rsa -f services/ssh/id_rsa -N ''
 cp services/java/uid.txt services/jupyter/uid.txt
 cp services/java/uid.txt services/redis/uid.txt
 cp services/java/uid.txt services/bitcore/uid.txt
+echo "Volumes created:"
 docker volume create --name cassandra
 docker volume create --name bitcore
 echo You can now build your enviroment running build.sh
