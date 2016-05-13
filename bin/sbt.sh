55f36cae0a4301659452a2f4e807fa86da06dd8a
Michele Sciabarra <michele+youtrack@sciabarra.com>
2016-05-13 14:37:11 +0200

    ammonite

--- bin/sbt.sh
+++ bin/sbt.sh
@@ -2,4 +2,4 @@
 cd $(dirname $0)
 B=$(dirname $PWD)
 cd $B
-bash $B/bin/sbt-paulp.sh -sbt-jar "$B/sbt-launch.jar" -sbt-boot "$B/project/boot" -ivy "$B/project/ivy2" "$@"
+bash $B/bin/psbt.sh -sbt-jar "$B/sbt-launch.jar" -sbt-boot "$B/project/boot" -ivy "$B/project/ivy2" "$@"
