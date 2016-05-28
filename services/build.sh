82e6dbc3394b3f726bb6c457550930cef2b3a2d3
Michele Sciabarra <michele@sciabarra.com>
2016-05-28 08:36:13 -0400

    removed old sample

--- services/build.sh
+++ services/build.sh
@@ -12,9 +12,9 @@ do
    cd $HERE/..
    if test -d apps/$i
    then rm services/$i/apps.tar
-	      git archive -o services/$i/apps.tar --format tar HEAD apps/$i
+	   git archive -o services/$i/apps.tar --format tar HEAD apps/$i
    fi
    cd $HERE
-   docker build -t sparkoin/$i-base:$V $i/base
-   docker build -t sparkoin/$i:$V $i
+   #docker build -t sparkoin/$i-base:$V $i/base
+   #docker build -t sparkoin/$i:$V $i
 done
